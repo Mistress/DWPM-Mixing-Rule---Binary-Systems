dbbc49f8c7ceb79215e231f63b85b71b6d3f915d
mistress <Mistressza@gmail.com>
2010-04-14 00:27:38 +0200

    fsolve multistart method wich yields good results
    * solves successfully for most systems, in most cases where solutions aren't
    	found it is due to "constraint" violation and not due to solver failure
    
    * One file in methanol-heptane (UNIQUAC) @ 313K which seems to violate
    	constraints, however it is unknown as to how this result is possible seeing
     	as the "f all(Test<0) and ier==1 and all(Pred_x_eq>=0) and all(Pred_x_eq<=1):"
    	in CalcPhaseStability should elliminate it from the possibilities

--- PhaseStability.py
+++ PhaseStability.py
@@ -5,21 +5,19 @@ import scipy.optimize
 
 def Tieline(x, ModelInstance, T, c, M):
     slope = (ModelInstance.deltaGmix(x[0], T, c, M) - ModelInstance.deltaGmix(x[1], T, c, M))/(x[0]-x[1])
-    Tangent = slope - ModelInstance.FirstDerivative(x[0], T, c, M)
-    return Tangent
+    return slope
         
 def System(x, ModelInstance, T, c, M):
     SlopeGmix = ModelInstance.FirstDerivative(x[0], T, c, M)-ModelInstance.FirstDerivative(x[1], T, c, M)
-    
     Tangent = Tieline(x, ModelInstance, T, c, M)-ModelInstance.FirstDerivative(x[0], T, c, M)
-    system = append(SlopeGmix, Tangent)
+    system = array([SlopeGmix, Tangent])
     return system
     
 
 def NonEqConstr(x, ModelInstance, T, c, M):
    
-    TangentTest = array([((ModelInstance.FirstDerivative(x[0], T, c,M))*(comp - x[0])+ ModelInstance.deltaGmix(x[0], T, c, M))-ModelInstance.deltaGmix(comp, T, c, M) for comp in arange(0, 1.001, 0.001)])
-    DistinctTest = -1*array([abs(x[0]-x[1])+0.1])
+    TangentTest = array([((ModelInstance.FirstDerivative(x[0], T, c,M))*(comp - x[0])+ ModelInstance.deltaGmix(x[0], T, c, M))-ModelInstance.deltaGmix(comp, T, c, M) for comp in arange(0, 1.01, 0.01)])
+    DistinctTest = -1*array([abs(x[0]-x[1])-0.1])
     return append(TangentTest, DistinctTest)
        
 
@@ -28,8 +26,8 @@ def NonEqConstr(x, ModelInstance, T, c, M):
 def CalcPhaseStability(ModelInstance, T, c, M):
    
     
-    dtype = [('x_Pred1', float), ('x_Pred2',float), ('f_value', float)]
-    OutArray = 100*array([ones(3)])
+    dtype = [('x_Pred1', float), ('x_Pred2',float), ('Equal_Slopes', float),('Tangent', float) ]
+    OutArray =array([[0, 1, 100, 100]])
 
     for x1 in arange(0, 1.05, 0.05):
         for x2 in arange(0, 1.05, 0.05):
@@ -38,11 +36,11 @@ def CalcPhaseStability(ModelInstance, T, c, M):
             (Pred_x_eq, infodict, ier, msg) = scipy.optimize.fsolve(System, xo,(ModelInstance, T, c, M), None, 1, 0, 10**-4, 0, None, 0.0, 100, None,True)
             
             Test = NonEqConstr(Pred_x_eq, ModelInstance, T, c, M)
-            if all(Test<0) & ier==1 :
-                OutArray = append(OutArray, [[Pred_x_eq[0], Pred_x_eq[1], sum(infodict['fvec']**2)]], 0)
+            if all(Test<0) and ier==1 and all(Pred_x_eq>=0) and all(Pred_x_eq<=1):
+                OutArray = append(OutArray, [[Pred_x_eq[0], Pred_x_eq[1], infodict['fvec'][0], infodict['fvec'][1]]], 0)
         
     OutList = [tuple(item) for item in OutArray]
     StructOutArray = array(OutList, dtype)
-    BestFit = sort(StructOutArray, order=['f_value'])
+    BestFit = sort(StructOutArray, order=['Equal_Slopes', 'Tangent'])
 
     return array([BestFit[0][0], BestFit[0][1]])
