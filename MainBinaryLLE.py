c8140267a4e264e0d67b035f8a2ede69c8208730
mistress <Mistressza@gmail.com>
2010-04-15 17:42:49 +0200

    Model fitting code with random multistart fsolve method for phase equilibrium calcs

--- MainBinaryLLE.py
+++ MainBinaryLLE.py
@@ -40,70 +40,41 @@ class Mixture:
             h5file = tables.openFile(PureDataDir+'/'+Compound+'.h5', 'r')
             properties = h5file.root.Properties
             self.Data[Compound] = dict(((field, row[field]) for row in properties.iterrows() for field in properties.colnames))        
+            h5file.close()
         self.Name = '-'.join(Compounds)
         h5file = tables.openFile(MixtureDataDir+'/'+ self.Name +'.h5', 'r')
-        self.M = dict(Compounds = h5file.root.Compounds.read(), ExpComp = h5file.root.ExperimentalData.TielineData.ExpComp.read(), T =h5file.root.ExperimentalData.TielineData.T.read())
+        self.M = dict(Compounds = h5file.root.Compounds.read(), ExpComp = h5file.root.ExperimentalData.T_CompData.ExpComp.read(), T =h5file.root.ExperimentalData.T_CompData.T.read())
         for row in h5file.root.UNIQUACParams.iterrows():
             for field in h5file.root.UNIQUACParams.colnames:
                 self.M[field] = row[field]
         self.vdWaalsInstance = vanDerWaals.Slope(self.Data, Compounds)
         [Slope, VPData] = self.vdWaalsInstance.BestFit()
         self.AdachiLuParam = dict((Compound,  Slope[Compound]['BestFit']) for Compound in Compounds)
+        h5file.close()
     
     def Plotter(self, BestParams, Model, Fit, ModelInstance, Actual, c, T):
-        print BestParams
-        print Model
-    
+       
         deltaGibbsmix = array([ModelInstance.deltaGmix(x, T, c, self.M) for x in arange(0.001, 1, 0.001)])
+        ActualPlot = array([ModelInstance.deltaGmix(x, T, c, self.M) for x in Actual])
         TangentComps = PhaseStability.CalcPhaseStability(ModelInstance, T, c, self.M)
         TangentGibbs = [ModelInstance.deltaGmix(x, T, c, self.M)for x in TangentComps]
         AbsError = ErrorClasses.SumAbs(TangentComps, Actual).Error()
         matplotlib.rc('text', usetex = True)
         fig = matplotlib.pyplot.figure()
-        matplotlib.pyplot.plot(arange(0.001, 1, 0.001), deltaGibbsmix, 'b-')
-        matplotlib.pyplot.plot(TangentComps, TangentGibbs, 'r-')
+        matplotlib.pyplot.plot(arange(0.001, 1, 0.001), deltaGibbsmix, 'b-',TangentComps, TangentGibbs, 'r-', Actual, ActualPlot, 'ko')
         matplotlib.pyplot.xlabel(r'Mole Fraction of '+self.Compounds[0].capitalize(), fontsize = 14)
         matplotlib.pyplot.ylabel(r'$\displaystyle\frac{\Delta G_{mix}}{RT}$', fontsize = 14)
         matplotlib.pyplot.title(r'\textbf{Predicted Phase Equilibrium at %3.2f}'%T, fontsize = 14)
         ax = fig.add_axes([0,0,1,1])
-        ax.text(0,0, 'Model: %s, Params: %s, Abslote Error(sum): %4.4e'%(Model, str(around(BestParams, 2)),AbsError), fontsize=12, transform=ax.transAxes)
+        ax.text(0,0, 'Model: %s, Params: %s, Abslote Error(sum): %4.4e'%(Model, str(around(BestParams, 4)),AbsError), fontsize=12, transform=ax.transAxes)
         ax.set_axis_off()
         #show()
         savefig('Results/'+self.Name+'/'+Model+'/'+ Fit+'/T_'+str(T) +'.pdf')
         matplotlib.pyplot.close()
-                   
-        if not(path.exists('Results/'+self.Name+'/'+ Model+'/'+Fit+'/'+self.Name +'.h5')):
-            h5file = tables.openFile('Results/'+self.Name+'/'+Model+'/'+Fit+'/'+self.Name +'.h5', 'w', "Optimization Outputs")
-            if Model=='DWPM':                                             
-                table = h5file.createTable("/", "Outputs", ResultsFile1, "Optimal model parameters, predicted phase equilibrium, errors etc")
-            else:
-                table = h5file.createTable("/", "Outputs", ResultsFile2, "Optimal model parameters, predicted phase equilibrium, errors etc")
-
-            table.row['T'] = T
-            table.row['ModelParams'] = array(BestParams)
-            table.row['Predicted'] = TangentComps
-            table.row['Actual'] = Actual
-            table.row['SumSqrError'] = ErrorClasses.SumSquare(TangentComps, Actual).Error()
-            table.row['AbsError'] = ErrorClasses.AbsError(TangentComps, Actual).Error()
-
-            table.row.append()
-            table.flush()
-            h5file.close()
-        else:
-            h5file = tables.openFile('Results/'+self.Name+'/'+Model+'/'+Fit+'/'+self.Name +'.h5', 'r+')
-            table = h5file.root.Outputs
-            table.row['T'] = T
-            table.row['ModelParams'] = array(BestParams)
-            table.row['Predicted'] = TangentComps
-            table.row['Actual'] = Actual
-            table.row['SumSqrError'] = ErrorClasses.SumSquare(TangentComps, Actual).Error()
-            table.row['AbsError'] = ErrorClasses.AbsError(TangentComps, Actual).Error()
-            
-            table.row.append()
-            table.flush()
-            h5file.close()
-
+        print self.Name, Actual, TangentComps
         
+                   
+              
             
            
     def OptFunctionIndvT(self, params, ModelInstance, Actual, T, c):
@@ -201,43 +172,43 @@ class Mixture:
        
 
 ##=============================================================##
-Models = ('DWPM', 'NRTL', 'UNIQUAC')
-ModelInstances = (GibbsClasses.DWPM, GibbsClasses.NRTL, GibbsClasses.UNIQUAC)
-MixtureDataDir = 'Data/Mixtures'
-PureDataDir = 'Data/PureComps'
-Compounds = ('1-butanol', 'water')
-Bounds = [((-1000, 0), (-1000, 0), (0.5, 0.5)), ((-800, 3000), (-800, 3000)), ((-800, 3000), (-800, 3000))]
-InitParams =[(-200.0,-25.0, 0.5), (-250.0, 1500.0), (-20, 300.00)]
-R = 8.314
+#Models = ('DWPM', 'NRTL', 'UNIQUAC')
+#ModelInstances = (GibbsClasses.DWPM, GibbsClasses.NRTL, GibbsClasses.UNIQUAC)
+#MixtureDataDir = 'Data/Mixtures'
+#PureDataDir = 'Data/PureComps'
+#Compounds = ('1-butanol', 'water')
+#Bounds = [((-1000, 0), (-1000, 0), (0.5, 0.5)), ((-800, 3000), (-800, 3000)), ((-800, 3000), (-800, 3000))]
+#InitParams =[(-200.0,-25.0, 0.5), (-250.0, 1500.0), (-20, 300.00)]
+#R = 8.314
 
-if not(path.exists('Results/')):
-    mkdir('Results/')   
+#if not(path.exists('Results/')):
+#    mkdir('Results/')   
 
 
-Optimization = Mixture(Compounds, MixtureDataDir, PureDataDir)     
-if not(path.exists('Results/'+Optimization.Name)):
-        mkdir('Results/'+Optimization.Name)
+#Optimization = Mixture(Compounds, MixtureDataDir, PureDataDir)     
+#if not(path.exists('Results/'+Optimization.Name)):
+#        mkdir('Results/'+Optimization.Name)
 
-for i in arange(size(Models)):  
-    Name = '-'.join(Compounds)
-    if path.exists('Results/'+Name+'/'+ Models[i]+'/IndividualT/'+Name +'.h5'):
-        remove('Results/'+Name+'/'+ Models[i]+'/IndividualT/'+Name +'.h5') 
-    
-    Optimization.BestFitParamsIndvT(Models[i], ModelInstances[i], InitParams[i], Bounds[i])
-    
-    h5file = tables.openFile('Results/'+Name+'/'+ Models[i]+'/IndividualT/'+Name +'.h5', 'r')
-    PlotT = array([row['T'] for row in h5file.root.Outputs.iterrows()])
-    PlotExpX = array([row['Actual'] for row in h5file.root.Outputs.iterrows()])
-    PlotPredX = array([row['Predicted'] for row in h5file.root.Outputs.iterrows()])
-    h5file.close()
-    matplotlib.rc('text', usetex = True)
-    fig = matplotlib.pyplot.figure()
-    matplotlib.pyplot.plot(PlotPredX, PlotT, 'r-', PlotExpX, PlotT, 'ko')
-    matplotlib.pyplot.xlabel(r'Mole Fraction of '+Compounds[0].capitalize(), fontsize = 14)
-    matplotlib.pyplot.ylabel(r'Temperature', fontsize = 14)
-    matplotlib.pyplot.title(r'\textbf{Predicted Phase Diagram}', fontsize = 14)
-    savefig('Results/'+Name+'/'+Models[i]+'/IndividualT/PhaseDiagram.pdf')
-    matplotlib.pyplot.close()
+#for i in arange(size(Models)):  
+#    Name = '-'.join(Compounds)
+#    if path.exists('Results/'+Name+'/'+ Models[i]+'/IndividualT/'+Name +'.h5'):
+#        remove('Results/'+Name+'/'+ Models[i]+'/IndividualT/'+Name +'.h5') 
+#    
+#    Optimization.BestFitParamsIndvT(Models[i], ModelInstances[i], InitParams[i], Bounds[i])
+#    
+#    h5file = tables.openFile('Results/'+Name+'/'+ Models[i]+'/IndividualT/'+Name +'.h5', 'r')
+#    PlotT = array([row['T'] for row in h5file.root.Outputs.iterrows()])
+#    PlotExpX = array([row['Actual'] for row in h5file.root.Outputs.iterrows()])
+#    PlotPredX = array([row['Predicted'] for row in h5file.root.Outputs.iterrows()])
+#    h5file.close()
+#    matplotlib.rc('text', usetex = True)
+#    fig = matplotlib.pyplot.figure()
+#    matplotlib.pyplot.plot(PlotPredX, PlotT, 'r-', PlotExpX, PlotT, 'ko')
+#    matplotlib.pyplot.xlabel(r'Mole Fraction of '+Compounds[0].capitalize(), fontsize = 14)
+#    matplotlib.pyplot.ylabel(r'Temperature', fontsize = 14)
+#    matplotlib.pyplot.title(r'\textbf{Predicted Phase Diagram}', fontsize = 14)
+#    savefig('Results/'+Name+'/'+Models[i]+'/IndividualT/PhaseDiagram.pdf')
+#    matplotlib.pyplot.close()
     
    # if path.exists('Results/'+Name+'/'+ Models[i]+'/OverallT/'+Name +'.h5'):
    #     remove('Results/'+Name+'/'+ Models[i]+'/OverallT/'+Name +'.h5') 
--- PhaseStability.py
+++ PhaseStability.py
@@ -4,27 +4,41 @@ from scipy import *
 import scipy.optimize
 
 def Tieline(x, ModelInstance, T, c, M):
-            slope = (ModelInstance.deltaGmix(x[0], T, c, M) - ModelInstance.deltaGmix(x[1], T, c, M))/(x[0]-x[1])
-            return slope
+    slope = (ModelInstance.deltaGmix(x[0], T, c, M) - ModelInstance.deltaGmix(x[1], T, c, M))/(x[0]-x[1])
+    return slope
         
 def System(x, ModelInstance, T, c, M):
-            SlopeGmix = ModelInstance.FirstDerivative(x[0], T, c, M)-ModelInstance.FirstDerivative(x[1], T, c, M)
-            Tangent = Tieline(x, ModelInstance, T, c, M)-ModelInstance.FirstDerivative(x[0], T, c, M)
-            system = append(SlopeGmix, Tangent)
+    SlopeGmix = ModelInstance.FirstDerivative(x[0], T, c, M)-ModelInstance.FirstDerivative(x[1], T, c, M)
+    Tangent = Tieline(x, ModelInstance, T, c, M)-ModelInstance.FirstDerivative(x[0], T, c, M)
+    system = array([SlopeGmix, Tangent])
+    return system
+    
 
-            return system
+def NonEqConstr(x, ModelInstance, T, c, M):
+   
+    TangentTest = array([((ModelInstance.FirstDerivative(x[0], T, c,M))*(comp - x[0])+ ModelInstance.deltaGmix(x[0], T, c, M))-ModelInstance.deltaGmix(comp, T, c, M) for comp in arange(0, 1.01, 0.01)])
+    DistinctTest = -1*array([abs(x[0]-x[1])-0.1])
+    return append(TangentTest, DistinctTest)
+       
 
 #===================================================================
        
 def CalcPhaseStability(ModelInstance, T, c, M):
-    xo = array([0.01, 0.99])
-    Pred_x_eq = scipy.optimize.fsolve(System, xo,(ModelInstance, T, c, M), None, 0, 0, 10**-4, 0, None, 0.0, 100, None,True)
-  
-    return Pred_x_eq
+   
+    
+    dtype = [('x_Pred1', float), ('x_Pred2',float), ('Equal_Slopes', float),('Tangent', float) ]
+    OutArray = array([0, 1, 100, 100])
+
+    for x1 in append(0.5*rand(20), 0.001):
+        for x2 in append(1-0.5*rand(20), 0.999):
+            
+            xo = array([x1, x2])
+            (Pred_x_eq, infodict, ier, msg) = scipy.optimize.fsolve(System, xo,(ModelInstance, T, c, M), None, 1, 0, 10**-6, 0, None, 0.0, 100, None,True)
+            
+            Test = NonEqConstr(Pred_x_eq, ModelInstance, T, c, M)
+            if all(Test<0) and ier==1 and all(Pred_x_eq>=0) and all(Pred_x_eq<=1) and (abs(infodict['fvec'][0])<OutArray[2] and abs(infodict['fvec'][1])<OutArray[3]):
+                OutArray = array([Pred_x_eq[0], Pred_x_eq[1], abs(infodict['fvec'][0]),abs(infodict['fvec'][1])])
         
     
-
-
-
-
-
+    BestFit = array([tuple(OutArray)], dtype)
+    return array([BestFit[0][0], BestFit[0][1]])
