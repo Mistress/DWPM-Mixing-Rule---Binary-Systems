68da7510ee02dd1a6fda2437cdf68abfefcb6dfa
Desiree Strauss <mistressza@gmail.com>
2011-08-08 13:03:43 +0200

    Attempt to find overall s-parameters to mimimize variance of binary interaction parameters

--- MainBinaryLLE.py
+++ MainBinaryLLE.py
@@ -94,7 +94,56 @@ class Mixture:
         table.flush()
         h5file.close()
 
-    def ParamCalc(self, Model, ModelParamsInstance, InitParamsLimit, Bounds, R, System):
+    def ModelsPlotter(self, Models, ModelInstances, Cell_s, alpha, z):
+
+        if path.exists('Results/'+self.Name+'/AllModelsGibbsPlots' ):
+            fileList = listdir('Results/'+self.Name+'/AllModelsGibbsPlots')
+            for fn in fileList: 
+                remove(path.join('Results/'+self.Name+'/AllModelsGibbsPlots', fn))
+        else:
+            mkdir('Results/'+self.Name+'/AllModelsGibbsPlots')
+
+            
+        for i in arange(size(Models)):
+
+            h5file = tables.openFile('Results/'+self.Name+'/'+ Models[i]+'/'+ self.Name +'.h5', 'r')
+            if Models[i]=="DWPM":
+                DWPMParams = array([row['ModelParams'] for row in h5file.root.Outputs.iterrows()])
+            elif Models[i]=="NRTL":
+                NRTLParams = array([row['ModelParams'] for row in h5file.root.Outputs.iterrows()])
+            elif Models[i]=="UNIQUAC":
+                UNIQUACParams = array([row['ModelParams'] for row in h5file.root.Outputs.iterrows()])
+            h5file.close()
+
+        for T in self.M['T']:
+            
+            CompC = self.vdWaalsInstance.CompC(T)
+            c = [CompC[Compound] for Compound in Compounds]
+            Actaul = array([self.M['ExpComp'][0], self.M['ExpComp'][1]])
+            ##ActualPlot = array([ModelInstance.deltaGmix(x, T, c, self.M) for x in Actual])
+
+            print append(array([interp(T, self.M['T'], DWPMParams[:, 0]), interp(T, self.M['T'], DWPMParams[:, 1]) ]), Cell_s)
+            DWPMInstance = ModelInstances[0](append(array([interp(T, self.M['T'], DWPMParams[:, 0]), interp(T, self.M['T'], DWPMParams[:, 1]) ]) , Cell_s))
+            DWPMPlot = array([DWPMInstance.deltaGmix(x, T, c, self.M) for x in arange(0.001, 1, 0.001)])
+
+            NRTLInstance = ModelInstances[1](array([interp(T, self.M['T'], NRTLParams[:, 0]), interp(T, self.M['T'], NRTLParams[:, 1]) ]))
+            NRTLPlot = array([NRTLInstance.deltaGmix(x, T, c, self.M) for x in arange(0.001, 1, 0.001)])
+
+            UNIQUACInstance = ModelInstances[2](array([interp(T, self.M['T'], UNIQUACParams[:, 0]), interp(T, self.M['T'], UNIQUACParams[:, 1]) ]))
+            UNIQUACPlot = array([UNIQUACInstance.deltaGmix(x, T, c, self.M) for x in arange(0.001, 1, 0.001)])
+
+            fig = matplotlib.pyplot.figure()
+            matplotlib.pyplot.plot(arange(0.001, 1, 0.001), DWPMPlot, 'b-', arange(0.001, 1, 0.001), NRTLPlot, 'r-', arange(0.001, 1, 0.001), UNIQUACPlot, 'k-')
+            matplotlib.pyplot.xlabel(r'Mole Fraction of '+self.Compounds[0].capitalize(), fontsize = 14)
+            matplotlib.pyplot.ylabel(r'$\displaystyle\frac{\Delta G_{mix}}{RT}$', fontsize = 14)
+            matplotlib.pyplot.title(r'\textbf{Predicted $\displaystyle\frac{\Delta G_{mix}}{RT}$ %3.2f}'%T, fontsize = 14)
+            #ax = fig.add_axes([0,0,1,1])
+            #ax.set_axis_off()
+
+            savefig('Results/'+self.Name+'/AllModelsGibbsPlots/T_'+str(T) +'.pdf')
+            matplotlib.pyplot.close()          
+            
+    def ParamCalc(self, Model, ModelInstance, InitParamsLimit, Bounds, R, System, Cell_s):
 
         if path.exists('Results/'+self.Name+'/'+Model):
             fileList = listdir('Results/'+self.Name+'/'+Model)
@@ -146,12 +195,15 @@ class Mixture:
                 c21 = Paramj[1]
                             
             Params = array([c12, c21])
-                        
-            ParamInstance = ModelParamsInstance(append(Params, Cell_s))
-            self.Plotter(Params, m, b, Cell_s, Model, ParamInstance, Actual, c, T, Converge)
 
-        return Params
+            if Model == "DWPM":
+                ParamInstance = ModelInstance(append(Params, Cell_s))
+                self.Plotter(Params, m, b, Cell_s, Model, ParamInstance, Actual, c, T, Converge)
+            else:
+                ParamInstance = ModelInstance(Params)
+                self.Plotter(Params, m, b, (), Model, ParamInstance, Actual, c, T, Converge)            
 
+             
     def PhaseDiagramCalc(self, Model, System, Params, R, T):
 
         CompC = self.vdWaalsInstance.CompC(T)
@@ -180,7 +232,6 @@ class Mixture:
             if (Phase[3]>0.01) or not(0.0 <= Phase[0]<=1.0) or not(0.0 <= Phase[1]<= 1.0) or (abs(Phase[0]-Phase[1])<0.1):
                 Converge = 3
         
-        print Mesg
         ##print NStarts
         ##print Phase
         InitPhase = Phase
@@ -191,15 +242,27 @@ class Mixture:
         b = Phase[3]
 
         return array([Predicted1, Predicted2])
-                                    
+
+    def OverallSOpt(self, Cell_s, Model, ModelInstance, InitParamsLimit, Bounds, R):
+
+        System = AnalyticSystemsClasses.SystemDWPM(Cell_s)
+        self.ParamCalc(Model, ModelInstance, InitParamsLimit, Bounds, R, System, Cell_s)
+        
+        h5file = tables.openFile('Results/'+self.Name+'/'+ Model+'/'+ self.Name +'.h5', 'r')
+        DWPMParams = array([row['ModelParams'] for row in h5file.root.Outputs.iterrows()])
+        h5file.close()
+        
+        return sum(abs(std(DWPMParams[:,0])),abs( std(DWPMParams[:,1])))
+                                   
 ##=============================================================##
 Models = ('DWPM', 'NRTL', 'UNIQUAC')
 ModelInstances = (GibbsClasses.DWPM, GibbsClasses.NRTL, GibbsClasses.UNIQUAC)
 MixtureDataDir = 'Data/Mixtures'
 PureDataDir = 'Data/PureComps'
 Bounds = ((0.00, 100), (-1000, 3000), (-800, 3000))
-Scale = ((15, 15), (4000, 4000), (3800, 3800))
 InitParamsLimit =((0.00, 0.15), (-100., 1000.), (-800., 3000.))
+alpha = 0.2
+z = 10
 R = 8.314
 
 if not(path.exists('Results/')):
@@ -219,25 +282,22 @@ for file in listdir(MixtureDataDir):
     for i in arange(size(Models)): 
 ##    for i in array([1]): 
         if Models[i] == "DWPM":
-            Cell_s = (0.3, 0.8)
+            InitCell_s = array([0.3, 0.8])
+            Cell_s = scipy.optimize.fmin(Optimization.OverallSOpt, InitCell_s,(Models[i], ModelInstances[i], InitParamsLimit[i], Bounds[i], R), xtol=0.01, ftol=0.001, maxiter=None, maxfun=None, full_output=0, disp=1, retall=0, callback=None)
             System = AnalyticSystemsClasses.SystemDWPM(Cell_s)
             PhaseSystem = AnalyticPhaseCompSystemsClasses.SystemDWPM(Cell_s)
         elif Models[i] == "NRTL":
-            Cell_s = ()
-            alpha = 0.2
             System = AnalyticSystemsClasses.SystemNRTL(alpha)
             PhaseSystem = AnalyticPhaseCompSystemsClasses.SystemNRTL(alpha)
         elif Models[i] == "UNIQUAC":
-            Cell_s = ()
-            z = 10
             System = AnalyticSystemsClasses.SystemUNIQUAC(Optimization.M, z)
             PhaseSystem = AnalyticPhaseCompSystemsClasses.SystemUNIQUAC(Optimization.M, z)
 
         Name = '-'.join(Compounds)
         print Models[i]
 
-        Optimization.ParamCalc(Models[i], ModelInstances[i], InitParamsLimit[i], Bounds[i], R, System)
-
+        Optimization.ParamCalc(Models[i], ModelInstances[i], InitParamsLimit[i], Bounds[i], R, System, Cell_s)
+        
         h5file = tables.openFile('Results/'+Name+'/'+ Models[i]+'/'+ Name +'.h5', 'r')
         PlotT = array([row['T'] for row in h5file.root.Outputs.iterrows()])
         PlotParams = array([row['ModelParams'] for row in h5file.root.Outputs.iterrows()])
@@ -264,19 +324,15 @@ for file in listdir(MixtureDataDir):
         LinearParams1 = scipy.interpolate.interp1d(PlotT, PlotParams[:,0])
         LinearParams2 = scipy.interpolate.interp1d(PlotT, PlotParams[:,1])
         
-      ##  CubicParams1 = scipy.interpolate.interp1d(PlotT, PlotParams[:,0], kind = 'cubic')
-      ##  CubicParams2 = scipy.interpolate.interp1d(PlotT, PlotParams[:,1], kind = 'cubic')
-
         InterpT = linspace(PlotT[0], PlotT[-1], 10)
-        
         PredictedLinear = array([Optimization.PhaseDiagramCalc(Models[i], PhaseSystem, array([LinearParams1(T), LinearParams2(T)]), R, T) for T in InterpT])
-      ##  PredictedCubic = array([Optimization.PhaseDiagramCalc(Models[i], PhaseSystem, array([CubicParams1(T), CubicParams2(T)]), R, T) for T in InterpT])
-
+      
         fig = matplotlib.pyplot.figure()
-      ##  matplotlib.pyplot.plot(PredictedLinear[:,:1], InterpT, 'r--', PredictedLinear[:,1:2], InterpT, 'r--', PredictedCubic[:,:1], InterpT, 'c-.', PredictedLinear[:,1:2], InterpT, 'c-.', PlotActual[:,:1], PlotT, 'ko', PlotActual[:,1:2], PlotT, 'ko')
         matplotlib.pyplot.plot(PredictedLinear[:,:1], InterpT, 'r--', PredictedLinear[:,1:2], InterpT, 'r--', PlotActual[:,:1], PlotT, 'ko', PlotActual[:,1:2], PlotT, 'ko')
         matplotlib.pyplot.ylabel(r'Temperature', fontsize = 14)
         matplotlib.pyplot.xlabel(r'Composition', fontsize = 14)
         matplotlib.pyplot.title(r'\textbf{Phase diagram for '+Optimization.Name.capitalize()+'}', fontsize = 14)
         savefig('Results/'+Name+'/'+Models[i]+'/PhaseDiagram.pdf')
         matplotlib.pyplot.close()
+
+ ##   Optimization.ModelsPlotter(Models, ModelInstances, Cell_s, alpha, z)
